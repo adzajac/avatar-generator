c7f48e6d210a15873c419a49334f80fc1a365777
work <adrianzdev@gmail.com>
2019-07-09 23:04:11 +0100

    colors for letter and background

--- app.py
+++ app.py
@@ -2,13 +2,15 @@ from PIL import ImageFont, ImageDraw, Image
 
 
 def genOneLetterAvatar(letter):
-    img_size = 1024
-    font_size = 1228
-    image = Image.new('RGBA', (img_size,img_size), (255,255,255,256))
+    image_size = 1024
+    font_size = 1228 
+    font_color = '#550000'
+    bkg_color = '#dddddd'
+    image = Image.new('RGBA', (image_size,image_size), font_color)
     d = ImageDraw.Draw(image)
     font = ImageFont.truetype("fonts/UbuntuMono-Bold.ttf", font_size)
-    d.text((203, -128), letter.upper, font=font, fill=(0,0,0,256))
+    d.text((203, -128), letter.upper(), font=font, fill=bkg_color)
     image.show()
     
-
+    
 genOneLetterAvatar('A')
