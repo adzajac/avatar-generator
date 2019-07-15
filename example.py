7469342ca661334a5cf0cd48b7c28fb3c5afa838
work <adrianzdev@gmail.com>
2019-07-15 22:40:47 +0100

    adding example of use

---
+++ example.py
@@ -0,0 +1,16 @@
+#import sys
+#sys.path = ['.'] + sys.path
+
+from flask import Flask
+
+import avatargenerator
+
+
+
+app = Flask(__name__)
+
+
+
+
+if __name__ == "__main__":
+    print("example")
