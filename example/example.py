3028f99ac4792d9966ade308c98b5b183e2a2e63
work <adrianzdev@gmail.com>
2019-07-17 22:16:10 +0100

    styles example

--- example/example.py
+++ example/example.py
@@ -2,17 +2,17 @@ import sys
 sys.path = [".."] + sys.path
 
 from flask import Flask, render_template, request, redirect, url_for
-from avatargenerator import avatargenerator as ag
-
 from flask_wtf import FlaskForm
 from wtforms import Form, StringField, SubmitField, validators
 from random import random
+from avatargenerator import avatargenerator as ag
 
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "ower-ads34-tndf-poe"
 
 
+# ---------- model -----------
 
 class User:
     first_name = ""
@@ -21,8 +21,8 @@ class User:
 
 user = User()
 
-#   routes
-#--------------
+
+# ---------- view -----------
 
 @app.route("/", methods=["post","get"])
 def index():
@@ -32,24 +32,18 @@ def index():
         user.second_name = form.second_name.data
         user.img_path = ag.generateLetterAvatar(user.first_name, user.second_name, size=512, dir_path="static/generated_avatars/")
         return redirect(url_for("success"))
-    return render_template("main.html", form=form)
+    return render_template("registration.html", form=form)
+
 
 @app.route("/success")
 def success():
     return render_template("success.html", user=user)
-    
+           
 
-    
-
-
-#    def __init__(self, first_name, second_name, img_path=""):
-#        self.first_name = first_name
-#        self.second_name = second_name
-#        self.img_paht = img_path
-        
+# ---------- other -----------
 
 class ExampleRegistrationForm(FlaskForm):
-    first_name = StringField("First Name", [validators.InputRequired()])
+    first_name = StringField("First Name (required)", [validators.InputRequired()])
     second_name = StringField("Second Name")
     submit = SubmitField("Register")
 
