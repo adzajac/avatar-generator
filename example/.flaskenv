a444e1cb145a350be86a94333eeb52cdedaf51e7
work <adrianzdev@gmail.com>
2019-07-17 19:06:22 +0100

    update example to work with the new code

--- example/.flaskenv
+++ example/.flaskenv
@@ -1,2 +1,2 @@
-FLASK_APP=app.py
+FLASK_APP=example.py
 FLASK_ENV=development
--- example/example.py
+++ example/example.py
@@ -1,7 +1,5 @@
 import sys
-sys.path = ['..'] + sys.path
-
-
+sys.path = [".."] + sys.path
 
 from flask import Flask, render_template, request, redirect, url_for
 from avatargenerator import avatargenerator as ag
@@ -12,45 +10,47 @@ from random import random
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'ower-ads34-tndf-poe'
+app.config["SECRET_KEY"] = "ower-ads34-tndf-poe"
+
+
 
 class User:
-    first_name = ''
-    second_name = ''
-    img_path = ''
+    first_name = ""
+    second_name = ""
+    img_path = ""
 
 user = User()
 
 #   routes
 #--------------
 
-@app.route('/', methods=['post','get'])
+@app.route("/", methods=["post","get"])
 def index():
     form = ExampleRegistrationForm()
     if form.validate():
         user.first_name = form.first_name.data
         user.second_name = form.second_name.data
-        user.img_path = ag.generateLetterAvatar(user.first_name, user.second_name, size=512, dir_path='static/generated_avatars/')
-        return redirect(url_for('success'))
-    return render_template('main.html', form=form)
+        user.img_path = ag.generateLetterAvatar(user.first_name, user.second_name, size=512, dir_path="static/generated_avatars/")
+        return redirect(url_for("success"))
+    return render_template("main.html", form=form)
 
-@app.route('/success')
+@app.route("/success")
 def success():
-    return render_template('success.html', user=user)
+    return render_template("success.html", user=user)
     
 
     
 
 
-#    def __init__(self, first_name, second_name, img_path=''):
+#    def __init__(self, first_name, second_name, img_path=""):
 #        self.first_name = first_name
 #        self.second_name = second_name
 #        self.img_paht = img_path
         
 
 class ExampleRegistrationForm(FlaskForm):
-    first_name = StringField('First Name', [validators.InputRequired()])
-    second_name = StringField('Second Name')
-    submit = SubmitField('Register')
+    first_name = StringField("First Name", [validators.InputRequired()])
+    second_name = StringField("Second Name")
+    submit = SubmitField("Register")
 
 
